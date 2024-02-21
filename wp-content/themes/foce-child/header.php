c2b13edb5b794168340186c6d6793840335fb42d
exploralgo <admin@exploralgo.fr>
2024-02-21 15:51:16 +0100

    animation menu burger

--- wp-content/themes/foce-child/header.php
+++ wp-content/themes/foce-child/header.php
@@ -12,48 +12,53 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
-    
-	<?php wp_head(); ?>
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'foce' ); ?></a>
+    <?php wp_body_open(); ?>
+    <div id="page" class="site">
+        <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'foce' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<nav id="site-navigation" class="main-navigation">
+        <header id="masthead" class="site-header">
+            <!-- 2 nav -->
+            <nav id="site-navigation" class="main-navigation">
 
-        <div class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
-
-
-            <input id="toggle" type="checkbox"></input>
-
-            <label for="toggle" class="hamburger">
-                <div class="top-bun"></div>
-                <div class="meat"></div>
-                <div class="bottom-bun"></div>
-            </label>
+                <div class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                        rel="home"><?php bloginfo( 'name' ); ?></a></div>
 
 
-            <div class="nav">
-                <div class="nav-wrapper">
-                    <nav>
-                    <ul>
-                        <li><a href="#story">Histoire</a></li>
-                        <li><a href="#characters">Personnages</a></li>
-                        <li><a href="#place">Lieu</a></li>
-                        <li><a href="#studio">Studio Koukaki</a></li>
-                    </ul>
-                    </nav>
+                <input id="toggle" type="checkbox"></input>
+
+                <label for="toggle" class="hamburger">
+                    <div class="top-bun"></div>
+                    <div class="meat"></div>
+                    <div class="bottom-bun"></div>
+                </label>
+
+
+                <div class="nav">
+                    <div class="nav-wrapper">
+                        <!-- <div class="nav-logo"><img src="/wp-content/themes/foce-child/assets/images/Image logo en paralax.png" alt=""></div> -->
+                        <nav>
+                            <ul>
+                                <li><a href="#story">Histoire</a></li>
+                                <li><a href="#characters">Personnages</a></li>
+                                <li><a href="#place">Lieu</a></li>
+                                <li><a href="#studio">Studio Koukaki</a></li>
+                            </ul>
+                        </nav>
+                        <!-- <div class="nav-name"><img src="wp-content/themes/foce-child/assets/images/Studio Koukaki.png" alt=""></div> -->
+                    </div>
                 </div>
-            </div>
 
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+            </nav><!-- #site-navigation -->
+        </header><!-- #masthead -->
